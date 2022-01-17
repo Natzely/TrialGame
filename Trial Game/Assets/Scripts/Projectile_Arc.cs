629eecce9f269e676356da5ac447af80da03813b
Edgar <edgalvazquez@gmail.com>
2022-01-17 11:12:56 -0600

    save for awayness

--- Trial Game/Assets/Scripts/Projectile_Arc.cs
+++ Trial Game/Assets/Scripts/Projectile_Arc.cs
@@ -11,19 +11,20 @@ public class Projectile_Arc : Projectile
     private Vector2 _destination;
     private bool _launch;
     private float _a;
+    private float _posHeightDif;
 
     public override void Launch(Vector2 destination)
     {
         _destination = destination;
         _arcVertex = new Vector2(
             (_destination.x + _startPos.x) / 2,
-            _startPos.y + ArcHeight);
+            _startPos.y + (ArcHeight + Mathf.Abs(_destination.y - _startPos.y)));
 
         float x = transform.position.x;
         float y = transform.position.y;
         _a = (y - _arcVertex.y) / Mathf.Pow((x - _arcVertex.x), 2);
         _launch = true;
-
+        _posHeightDif = Mathf.Abs(_destination.y - transform.position.y);
     }
 
     protected override void Awake()
@@ -45,7 +46,7 @@ public class Projectile_Arc : Projectile
         if (_launch)
         {
             float newX = Mathf.MoveTowards(transform.position.x, _destination.x, Speed * Time.deltaTime);
-            float newY = _a * Mathf.Pow((newX - _arcVertex.x), 2) + _arcVertex.y;
+            float newY = _a * Mathf.Pow((newX - _arcVertex.x), 2) + (_arcVertex.y);
             transform.position = new Vector2(newX, newY);
         }
     }
